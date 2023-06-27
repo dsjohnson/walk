70f9503f204a89e450c945da7fd2bff70d98917a
Devin Johnson <devin.johnson@noaa.gov>
2023-06-27 12:41:06 -0600

    passed telem_to_ras testing

--- R/telem_to_ras.R
+++ R/telem_to_ras.R
@@ -1,7 +1,7 @@
 
 #' @title Convert {ctmm} telemtry data object to a {terra} `SpatRaster` stack
 #' @param data A `telemetry` object from the {ctmm} package
-#' @param cov A `\link[terra]{SpatRaster}` stack of covariates that will be used in CTMC 
+#' @param ras A `\link[terra]{SpatRaster}` stack of covariates that will be used in CTMC 
 #' movement modeling. Cells with `NA` values will be considered areas to the animal cannot travel, i.e., 
 #' likelihood surfaces will be `0` for those cells.
 #' @param return_type Type of object returned. One if `"data.frame"`, `"sparse"` (sparse matrix), 
@@ -21,11 +21,11 @@
 #' @importFrom ctmm uere
 #' @importFrom methods as
 #' @export
-telem_to_sparse <- function(data, cov, return_type="sparse", max_err=NULL, trunc=1.0e-8){
+telem_to_ras <- function(data, ras, return_type="sparse", max_err=NULL, trunc=1.0e-8){
   # Check arguments
   if(!inherits(data, "telemetry")) stop("'data' must be a ctmm::telemery object!")
-  if(!inherits(cov, "SpatRaster")) stop("'cov' must be a terra::SpatRaster object!")
-  if(is.lonlat(cov)) stop("'cov' must be projected with units in meters.")
+  if(!inherits(ras, "SpatRaster")) stop("'ras' must be a terra::SpatRaster object!")
+  if(is.lonlat(ras)) stop("'ras' must be projected with units in meters.")
   
   # Evaluate error covariance
   err_nms <- c("COV.x.x", "COV.x.y", "COV.y.y")
@@ -45,61 +45,64 @@ telem_to_sparse <- function(data, cov, return_type="sparse", max_err=NULL, trunc
   }
   
   # Extract error neighborhood and get cells 
-  cov_prj <- crs(cov, proj=TRUE)
+  ras_prj <- crs(ras, proj=TRUE)
   telem_pts <- terra::vect(cbind(data$longitude, data$latitude), crs="epsg:4326") |>
-    terra::project(cov_prj)
+    terra::project(ras_prj)
   if(is.null(max_err)) max_err <- 4*sqrt(pmax(cov_xx,cov_yy))
   t_buf <- terra::buffer(telem_pts, max_err)
-  t_err <- terra::cells(cov, t_buf, touches=TRUE) 
+  t_err <- terra::cells(ras, t_buf, touches=TRUE) 
   t_err <- split(t_err[,'cell'], t_err[,'ID'])
   xy <- terra::crds(telem_pts) 
   
   lik_list <- vector("list", nrow(xy))
-  
-  t_err <- t_err |> bind_cols(get_corner(t_err$cell, cov, "ur"))
-  t_err <- t_err |> bind_cols(get_corner(t_err$cell, cov, "ll"))
-  
   out <- NULL
+  zero_ind <- FALSE
+  ras_val <- values(ras)
+  
   for(i in 1:nrow(xy)){
     # i <- 1
     sigma <- matrix(c(cov_xx[i], cov_xy[i], cov_xy[i], cov_yy[i]), 2, 2)
     mean <- as.vector(xy[i,])
-    lower <- get_corner(t_err[[i]], cov, "ll")
-    upper <- get_corner(t_err[[i]], cov, "ur")
+    lower <- get_corner(t_err[[i]], ras, "ll")
+    upper <- get_corner(t_err[[i]], ras, "ur")
     dfi <- cbind(obs=i, cell=t_err[[i]], lik=NA)
     dfi[,'lik'] <- sapply(1:length(t_err[[i]]), 
-                          \(j) pmvnorm(lower=lower[j,], upper=upper[j,], mean=mean, sigma=sigma)[1]
+                          \(j) pmvnorm(lower=lower[j,], upper=upper[j,], mean=mean, sigma=sigma, keepAttr=FALSE)
     )
+    m <- ifelse(is.na(ras_val[t_err[[i]]]), 0, 1)
+    dfi[,3] <- dfi[,3]*m
     dfi[,3] <- dfi[,3]/sum(dfi[,3])
     dfi[,3] <- ifelse(dfi[,3]<trunc, 0, dfi[,3])
     dfi <- dfi[dfi[,3]>0,,drop=FALSE]
     dfi[,3] <- dfi[,3]/sum(dfi[,3])
+    if(sum(dfi[,3]) <=sqrt(.Machine$double.eps)) zero_ind <- TRUE
     out <- rbind(out, dfi)
-    
-    if(return_type=="data.frame"){
-      return(as.data.frame(out))
-    } else if(return_type=="sparse"){
-      M <- Matrix::sparseMatrix(i = out[,'obs'], j = out[,'cell'], x = out[,'lik'], dims = c(nrow(xy), prod(dim(cov)[1:2])))
-      M <- as(M, "dgCMatrix")
-      return(M)
-    } else if(return_type=="matrix_df"){
-      return(out)
-    } else if(return_type=="dense"){
-      M <- Matrix::sparseMatrix(i = out[,'obs'], j = out[,'cell'], x = out[,'lik'], dims = c(nrow(xy), prod(dim(cov)[1:2])))
-      M <- as.matrix(M)
-      return(M)
-    } else{
-      warning("Unknown 'return_type' returning 'matrix_df'")
-      return(out)
-    }
-    
+  }
+  
+  if(zero_ind) warning("Some observations have likelihood values of 0 in all ras cells!")
+  
+  if(return_type=="data.frame"){
+    return(as.data.frame(out))
+  } else if(return_type=="sparse"){
+    M <- Matrix::sparseMatrix(i = out[,'obs'], j = out[,'cell'], x = out[,'lik'], dims = c(nrow(xy), prod(dim(ras)[1:2])))
+    M <- as(M, "dgCMatrix")
+    return(M)
+  } else if(return_type=="matrix_df"){
+    return(out)
+  } else if(return_type=="dense"){
+    M <- Matrix::sparseMatrix(i = out[,'obs'], j = out[,'cell'], x = out[,'lik'], dims = c(nrow(xy), prod(dim(ras)[1:2])))
+    M <- as.matrix(M)
+    return(M)
+  } else{
+    warning("Unknown 'return_type' returning 'matrix_df'")
+    return(out)
   }
   
   # 
-  # Possible extention:
+  # Possible extension for doing this in C++ directly:
   # https://stackoverflow.com/questions/51290014/rcpp-implementation-of-mvtnormpmvnorm-slower-than-original-r-function
   #  source code: https://rdrr.io/cran/mvtnorm/src/R/mvt.R
-  
+  #
   
 }
 
---
+++ testing/functions/telem_to_ras_test.R
@@ -0,0 +1,28 @@
+library(ctmm)
+# library(walk)
+library(terra)
+devtools::load_all(".") #load walk functions
+
+data(pelican)
+
+data <- ctmm::tbind(pelican)
+data$COV.x.x <- ifelse(data$COV.x.x==Inf, 20^2, data$COV.x.x)
+data$COV.y.y <- ifelse(data$COV.y.y==Inf, 20^2, data$COV.y.y)
+
+xy <- as.matrix(data[,c("x","y")])
+
+sa <- terra::rast(
+  xmin=min(xy[,1])-1000, xmax=max(xy[,1])+1000,
+  ymin=min(xy[,2])-1000, ymax=max(xy[,2])+1000,
+  crs=projection(data), ncol=100, nrow=100
+)
+
+telem_pts <- terra::vect(cbind(data$longitude, data$latitude), crs="epsg:4326") |>
+  terra::project(sa)
+t_buf <- terra::buffer(telem_pts, 300000)
+
+
+ras <- terra::rasterize(t_buf, sa)
+
+
+sp_data <- walk::telem_to_ras(data, ras)
