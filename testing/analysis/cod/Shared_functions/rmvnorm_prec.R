d62129fb58d9a1d1a02bc1461b499c1906515c37
Devin Johnson <devin.johnson@noaa.gov>
2024-03-12 18:18:04 -0600

    remove testing

--- testing/analysis/cod/Shared_functions/rmvnorm_prec.R
+++
@@ -1,8 +0,0 @@
-rmvnorm_prec <- function(mu, prec, n.sims) {
-  z <- matrix(rnorm(length(mu) * n.sims), ncol=n.sims)
-  L <- Matrix::Cholesky(prec, super=TRUE)
-  z <- Matrix::solve(L, z, system = "Lt") ## z = Lt^-1 %*% z
-  z <- Matrix::solve(L, z, system = "Pt") ## z = Pt    %*% z
-  z <- as.matrix(z)
-  return(mu + z)
-}
--- testing/tmb/test2.R
+++
@@ -1,3 +0,0 @@
-library(TMB)
-x <- rnorm(100,2,5)
-walk:::fit_norm(x)
